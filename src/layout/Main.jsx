087f608ccf58c5520b156dd273f13bbd71f72e0a
Sattarov Ramil <sattarovramil1@gmail.com>
2025-01-12 19:49:44 +0300

    gh api

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import { Movies } from "../components/Movies";
 import { Search } from "../components/Search";
 import { Preloader } from "../components/Preloader";
-
-const API_KEY = process.env.REACT_APP_API_KEY;
-
 class Main extends React.Component {
   state = {
     movies: [],
@@ -13,7 +10,7 @@ class Main extends React.Component {
 
   componentDidMount() {
     console.log(process.env);
-    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+    fetch(`https://www.omdbapi.com/?apikey=575555b0&s=matrix`)
         .then((response) => response.json())
         .then((data) =>
             this.setState({ movies: data.Search, loading: false })
@@ -27,7 +24,7 @@ class Main extends React.Component {
 searchMovies = (str, type = 'all') => {
     this.setState({ loading: true });
     fetch(
-        `https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
+        `https://www.omdbapi.com/?apikey=575555b0&s=${str}${
             type !== 'all' ? `&type=${type}` : ''
         }`
     )
